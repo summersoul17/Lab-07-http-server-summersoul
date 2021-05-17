702592354893b18346edd8582762b8a3ecc22a6a
Luos Remmus <74209218+summersoul17@users.noreply.github.com>
2021-05-17 11:09:04 +0300

    Create suggestion.hpp

---
+++ include/suggestion.hpp
@@ -0,0 +1,43 @@
+#ifndef INCLUDE_SUGGESTION_HPP_
+#define INCLUDE_SUGGESTION_HPP_
+#include <shared_mutex>
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <unistd.h>
+
+using json = nlohmann::json;
+using string = std :: string;
+
+struct sug_config{
+  string id;
+  string name;
+  int cost;
+};
+
+struct sug_answer{
+  string text;
+  int position;
+};
+
+class suggestion {
+ public:
+  explicit suggestion(string file_name) : file_name_(file_name){}
+
+  json get_json_suggestion(string input);
+
+  void update_suggestion();
+
+ private:
+  json data;
+
+  std::shared_mutex mutex;
+
+  std::vector<sug_config> suggestions;
+
+  string file_name_;
+};
+#endif  // INCLUDE_SUGGESTION_HPP_
